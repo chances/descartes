b8f1c9217e5bc4fca3e7ef96bf3032aae3424735
Chance Snow <git@chancesnow.me>
2021-01-11 03:51:21 -0600

    Generate a stub header for C interop

---
+++ ffi/descartes.h
@@ -0,0 +1,21 @@
+/* Generated with cbindgen:0.16.0 */
+
+/* DO NOT MODIFY THIS MANUALLY! This file was generated using cbindgen.
+ * To generate this file:
+ *   1. Get the latest cbindgen using `cargo install --force cbindgen`
+ *      a. Alternatively, you can clone `https://github.com/eqrion/cbindgen` and use a tagged release
+ *   2. Run `rustup run nightly cbindgen --lockfile Cargo.lock --crate descartes -o ffi/descartes.h`
+ */
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef float DESCARTES_N;
+
+#define DESCARTES_THICKNESS 0.001
+
+#define DESCARTES_MIN_LINE_LENGTH 0.01
+
+#define DESCARTES_MIN_ARC_LENGTH DESCARTES_MIN_LINE_LENGTH
